3619ffaf83900b3d5f71fd9b6aa836196bba3e7d
YaronWhite86 <yaronwhite86@gmail.com>
2017-04-24 19:02:50 +0300

    Add files via upload

--- commands.h
+++ commands.h
@@ -5,32 +5,31 @@
 #include <time.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <iostream>
+#include <deque>
+#include <list>
+#include <string>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #define MAX_LINE_SIZE 80
 #define MAX_ARG 20
+#define MAX_ARG 20
+#define MAX_HISTORY 50
 typedef enum { FALSE , TRUE } bool;
+typedef enum {FAILURE, SUCCESS} Result;
+char* current_path;
+char* previous_path = NULL;
+
+
 int ExeComp(char* lineSize);
 int BgCmd(char* lineSize, void* jobs);
 int ExeCmd(void* jobs, char* lineSize, char* cmdString);
 void ExeExternal(char *args[MAX_ARG], char* cmdString);
 
-class History
-{
-	public:
-	History();		
-	virtual ~History();	
-	void setPWDs(std::string PWD);	
-	protected:
-	std::string prevPWD;
-	std::string PWD;
-}
-
-class Jobs
-{
-	public:
-	protected:
-}
+class cmdHistory {
+public:
+	cmdHistory();
+};
 #endif
 
