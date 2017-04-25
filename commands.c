78dcf9e39f4ddc04d4f9c81352c5a8079bb239a6
YaronWhite86 <yaronwhite86@gmail.com>
2017-04-25 16:13:44 +0300

    Add files via upload

--- commands.c
+++ commands.c
@@ -1,6 +1,24 @@
 //		commands.c
 //********************************************
+#include <unistd.h> 
+#include <stdio.h>
+#include <time.h>
+#include <iostream>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#define MAX_LINE_SIZE 80
+#define MAX_ARG 20
+#define MAX_HISTORY_SIZE 50
+typedef enum { FALSE, TRUE } bool;
+typedef enum { FOREGROUND, BACKGROUND, INTERRUPTED } currState;
+typedef enum { FAILURE, SUCCESS } Result;
 #include "commands.h"
+
+cmdHistory CommandHistory;
+
 //********************************************
 // function name: ExeCmd
 // Description: interperts and executes built-in commands
@@ -12,7 +30,9 @@ int ExeCmd(void* jobs, char* lineSize, char* cmdString)
 	char* cmd; 
 	char* args[MAX_ARG];
 	char pwd[MAX_LINE_SIZE];
-	char* delimiters = " \t\n";  
+	char* delimiters = " \t\n";
+	
+	
 	int i = 0, num_arg = 0;
 	bool illegal_cmd = FALSE; // illegal command
     	cmd = strtok(lineSize, delimiters);
@@ -31,9 +51,25 @@ int ExeCmd(void* jobs, char* lineSize, char* cmdString)
 // ARE IN THIS CHAIN OF IF COMMANDS. PLEASE ADD
 // MORE IF STATEMENTS AS REQUIRED
 /*************************************************/
-	if (!strcmp(cmd, "cd") ) 
+	if (!strcmp(cmd, "cd") ) // change directory
 	{
-		
+		if (num_arg != 1)	illegal_cmd = TRUE;
+		else
+		{
+			if (!strcmp(args[1], "-"))
+			{
+				CommandHistory.swapCurrAndPrev();
+			}
+			else if (chdir(args[1]) == 0)//successfully changed directories
+			{
+				CommandHistory.updateHistory(args[1]);//or getcwd?
+			}
+			else
+			{
+				printf("smash error:> %s -path not found\n", args[1]);
+				return 1;
+			}
+		}
 	} 
 	
 	/*************************************************/
@@ -166,3 +202,73 @@ int BgCmd(char* lineSize, void* jobs)
 	return -1;
 }
 
+//**************************************************************************************
+// function name: History
+// Description: Constructor
+// Parameters: none
+// Returns: history object initialized to empty history, and PWD, and LastPWD are set
+//**************************************************************************************
+cmdHistory::cmdHistory() : historyCount(MAX_HISTORY_SIZE), PWD(""), prevPWD("")
+{
+	char pwd_buff[MAX_LINE_SIZE];//in case of failure, save in temp buffer
+	if (getcwd(pwd_buff, MAX_LINE_SIZE + 1) == NULL)
+	{
+		perror("smash error : > Can't find current working directory"); //send error message
+	}
+	else
+	{
+		PWD = (std::string)pwd_buff;
+		LastPWD = PWD;
+	}
+}
+
+
+//**************************************************************************************
+// function name: updateHistory
+// Description: add the last cmd to the history, including illegal/failed ones
+// Parameters: command string
+// Returns: none
+//**************************************************************************************
+void cmdHistory::updateHistory(std::string cmdString)
+{
+
+	if (cmdString.empty())	return;
+
+	cmdHistoryArr.push_back(cmdString);
+	if ((int)HistoryList.size() > maxhistory) HistoryList.pop_front();
+}
+
+//**************************************************************************************
+// function name: printHistory
+// Description: print all commands in our history array
+// Parameters: none
+// Returns: none
+//**************************************************************************************
+void cmdHistory::print()
+{
+	for (int i = 0; i < this->historyCount; i++) {
+		printf("%c\n", this->cmdHistoryArr[i]);
+	}
+}
+
+//**************************************************************************************
+// function name: setPWDandLastPWD
+// Description: update the PWD and the LastPWD
+// Parameters: newPWD
+// Returns: none
+//**************************************************************************************
+void History::setPWDandLastPWD(std::string newPWD)
+{
+	LastPWD = PWD;
+	//PWD = newPWD;
+	char pwd_buff[MAX_LINE_SIZE];//in case of failure, save in temp buffer
+	if (getcwd(pwd_buff, MAX_LINE_SIZE + 1) == NULL)
+	{
+		perror("smash error : > Can't find current working directory"); //send error message
+		PWD = "";
+	}
+	else
+	{
+		PWD = (std::string)pwd_buff;
+	}
+}
--- commands.h
+++ commands.h
@@ -3,46 +3,180 @@
 #include <unistd.h> 
 #include <stdio.h>
 #include <time.h>
+#include <iostream>
 #include <stdlib.h>
 #include <signal.h>
-#include <iostream>
-#include <deque>
-#include <list>
-#include <string>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #define MAX_LINE_SIZE 80
 #define MAX_ARG 20
-#define MAX_ARG 20
-#define MAX_HISTORY 50
-typedef enum { FALSE , TRUE } bool;
-typedef enum {FAILURE, SUCCESS} Result;
-char* current_path;
-char* previous_path = NULL;
+#define MAX_HISTORY_SIZE 50
+typedef enum { FALSE, TRUE } bool;
+typedef enum { FOREGROUND, BACKGROUND, INTERRUPTED } currState;
+typedef enum { FAILURE, SUCCESS } Result;
 
+using namespace std;
 
-int ExeComp(char* lineSize);
-int BgCmd(char* lineSize, void* jobs);
-int ExeCmd(void* jobs, char* lineSize, char* cmdString);
+int ExeComp(char* lineSize, char* cmdString);
+int BgCmd(char* lineSize, char* cmdString);
+int ExeCmd(char* lineSize, char* cmdString);
 void ExeExternal(char *args[MAX_ARG], char* cmdString);
 
+//**************************************************************************************
+// Class Name: cmdHistory
+// cmdHistory object holds previous commands.
+// Has the ability to: update and print its contents. 
+//**************************************************************************************
 class cmdHistory
 {
-	public:
-	cmdHistory();		
-	virtual ~cmdHistory();	
-	void setPWDs(std::string PWD);	
-	protected:
-	std::string prevPWD;
+public:
+	cmdHistory();
+	~cmdHistory();
+	std::string get_pwd()
+	{
+		return PWD;
+	}
+	std::string get_prev_pwd()
+	{
+		return prevPWD;
+	}
+	void print();
+	void updateHistory(std::string cmdString);
+	void updateCurrAndPrev(std::string new_pwd);
+	void swapCurrAndPrev();
+
+private:
+	std::string cmdHistoryArr[MAX_HISTORY_SIZE];
 	std::string PWD;
+	std::string prevPWD;
+	const int historyCount;
+};
+
+//**************************************************************************************
+// Class Name: Job
+// According to the definition of the project Job objects will contain:
+// PID,NAME,TIME SPENT IN JOB LIST, WHERE THE JOB IS CURRENTLY RUNNING (BG,FG,INTERRUPTED)
+//**************************************************************************************
+class Job
+{
+public:
+	Job() : pid(-1), name(""), TimeInList((time_t)0), curr_state(background) {}; //default overwrite
+	Job(int pid_, std::string name, time_t time, currState state) : pid(pid_), jobname(name), TimeInList(time), currState(state) {};
+	virtual ~Job() {};
+
+	int pid;
+	std::string name;
+	time_t TimeInList;
+	currState curr_state
+};
+/* started by using struct but easier to use class.
+typedef struct _Job {
+	int pid;
+	std::string name;
+	time_t startingTime;
+	currState curr_state;
+};*/
+
+
+//**************************************************************************************
+// Name: cmdHistory
+// Description: Constructor
+// Parameters: none
+// Returns: Initialized object
+//**************************************************************************************
+cmdHistory::cmdHistory() : historyCount(0), PWD(""), prevPWD("")
+{
+	char buffer[MAX_LINE_SIZE];//in case of failure, save in temp buffer
+	if (getcwd(buffer, MAX_LINE_SIZE + 1) == NULL)
+	{
+		perror("smash error : > current working directory does not exist"); //send error message
+	}
+	else
+	{
+		PWD = (std::string)buffer;
+		prevPWD = PWD;
+	}
 }
 
-class Jobs
+
+//**************************************************************************************
+// Name: updateHistory
+// Description: updates our history array by adding the current command and using FIFO
+//				protocol if our array is full. 
+// Parameters: string cmdString
+// Returns: none
+//**************************************************************************************
+void cmdHistory::updateHistory(std::string cmdString)
 {
-	public:
-	protected:
+	
+	if (cmdString.empty())	return;
+	if (historyCount == MAX_HISTORY_SIZE) // if history array is full we use FIFO
+	{
+		for (int i = 0; i<MAX_HISTORY_SIZE-1; i++)
+		{
+			cmdHistoryArr[i] = cmdHistoryArr[i + 1];
+		}
+		cmdHistoryArr[MAX_HISTORY_SIZE] = cmdString;
+	}
+	else
+	{
+		cmdHistoryArr[historyCount] = cmdString;
+		historyCount++;
+	}
+}
+
+//**************************************************************************************
+// Name: printHistory
+// Description: print all commands in our history array
+// Parameters: none
+// Returns: none
+//**************************************************************************************
+void cmdHistory::print()
+{
+	for (int i = 0; i < historyCount; i++) {
+		printf("%c\n", cmdHistoryArr[i]);
+	}
+}
+
+//**************************************************************************************
+// Name: updateCurrAndPrev
+// Description: update the PWD and the previous PWD
+// Parameters: string new_pwd
+// Returns: none
+//**************************************************************************************
+void cmdHistory::updateCurrAndPrev(std::string new_pwd)
+{
+	prevPWD = PWD;
+	//PWD = newPWD;
+	char buffer[MAX_LINE_SIZE];//in case of failure, save in temp buffer
+	if (getcwd(buffer, MAX_LINE_SIZE + 1) == NULL)
+	{
+		perror("smash error : > current working directory does not exist"); //send error message
+		PWD = "";
+	}
+	else
+	{
+		PWD = (std::string)buffer;
+	}
+}
+
+//**************************************************************************************
+// Name: swapCurrAndPrev
+// Description: in case of "-" we simply swap the current and previous PWD
+// Parameters: none
+// Returns: none
+//**************************************************************************************
+void cmdHistory::swapCurrAndPrev()
+{
+	std::string temp = PWD;
+	if (PWD == "" || prevPWD == "")
+	{
+		perror("smash error : > can't swap non-existant PWD's");
+		return;
+	}
+	PWD = prevPWD;
+	prevPWD = temp;
 }
 
 #endif
-
